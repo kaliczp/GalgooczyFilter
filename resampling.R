36380aaf34e9cd63e749d5f9cea6dc445b785860
Péter Kalicz <kaliczp@gmail.com>
2022-08-03 11:26:15 +0200

    5 min frequency with additional data points

--- resampling.R
+++ resampling.R
@@ -1,2 +1,9 @@
 Sys.setenv(TZ='UTC')
 Qmod.xts <- xts(coredata(Q.xts), round(index(Q.xts)) + 13)
+
+time5min <- seq(as.POSIXct("2009-06-11 05:05:00"),as.POSIXct("2009-06-12 05:40:00"), by = "5 mins")
+xts5min <- xts(as.numeric(rep(NA, length(time5min))), time5min)
+Qmod5min <- c(xts5min, Q.xts)
+select <- is.na(Qmod5min)
+Qmod5min <- na.approx(Qmod5min)
+Qmod5minOK <- Qmod5min[select]
