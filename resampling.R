d641193bf896b472db0eba863693c4a8aa92632e
Péter Kalicz <kaliczp@gmail.com>
2022-08-03 12:15:21 +0200

    Resampling 15 min, control plot modified accordingly
    Resampling for 15 min is created.
    
    Control plot shows all cases. A legend is created for
    better understanding.

--- resampling.R
+++ resampling.R
@@ -17,7 +17,17 @@ select <- is.na(Qmod10min)
 Qmod10min <- na.approx(Qmod10min)
 Qmod10minOK <- Qmod10min[select]
 
+## 15min
+time15min <- seq(as.POSIXct("2009-06-11 05:05:00"),as.POSIXct("2009-06-12 05:35:00"), by = "15 mins")
+xts15min <- xts(as.numeric(rep(NA, length(time15min))), time15min)
+Qmod15min <- c(xts15min, Q.xts)
+select <- is.na(Qmod15min)
+Qmod15min <- na.approx(Qmod15min)
+Qmod15minOK <- Qmod15min[select]
+
 ## Control plot
 plot.zoo(Q.xts)
 lines(as.zoo(Qmod5minOK), col = 2)
 lines(as.zoo(Qmod10minOK), col = 3)
+lines(as.zoo(Qmod15minOK), col = 4)
+legend("topright", c("Original", "5 min", "10 min", "15 min"), lwd = 1, col = 1:4)
